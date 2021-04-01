7c484b2da42dc0911a08c85efd076c9b833863c0
zansa <sara.zanchi@dvisionlab.com>
2021-04-01 09:29:56 +0200

    feat: improvements

--- library/auth/components/AccountPanel.vue
+++ library/auth/components/AccountPanel.vue
@@ -1,12 +1,6 @@
 <template>
   <!-- personal account panel -->
-  <v-menu
-    v-if="user"
-    :offset-x="offsetX"
-    :offset-y="offsetY"
-    open-on-hover
-    tile
-  >
+  <v-menu v-if="user" open-on-hover tile v-bind="options">
     <template v-slot:activator="{ on, attrs }">
       <slot v-bind="{ on, attrs }">
         <!-- default slot content -->
@@ -22,7 +16,7 @@
       </slot>
     </template>
 
-    <v-card flat tile>
+    <v-card color="grey" flat tile>
       <v-card-title class="d-inline-block">
         <span class="text-capitalize">{{ user.firstname }}</span>
         &nbsp;
@@ -44,17 +38,36 @@
 export default {
   name: "AccountPanel",
   props: {
+    direction: { required: false, type: String },
     icon: { default: "mdi-account", type: String },
     label: { default: "account", type: String },
-    offsetX: { default: true, type: Boolean },
-    offsetY: { default: false, type: Boolean },
-    settingsRoute: { required: false, type: String }
+    settingsRoute: { required: false, type: String },
+    title: { default: "account", type: String }
   },
   computed: {
     iconColor() {
       return this.settingsRoute && this.$route.name == this.settingsRoute
         ? "accent"
         : "inherit";
+    },
+    options() {
+      let result = {};
+
+      switch (this.direction) {
+        case "bottom":
+          result["offset-y"] = true;
+          break;
+
+        case "right":
+          result["offset-x"] = true;
+          result["top"] = true;
+          break;
+
+        default:
+          break;
+      }
+
+      return result;
     },
     user() {
       return this.$store.state.auth.user;
