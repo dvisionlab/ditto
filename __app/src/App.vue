b69ec1fbb0c50723678e5b6937ab117c6a112d28
zansa <sara.zanchi@dvisionlab.com>
2021-04-12 09:31:38 +0200

    wip

---
+++ __app/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>{{ name }}</h1>
+  </div>
+</template>
+
+<script>
+import { name } from "@/../package.json";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      name
+    };
+  }
+};
+</script>
