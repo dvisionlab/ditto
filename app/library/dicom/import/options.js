b21fbac81de6518bd3fd6428703d99adb8843263
zansa <sara.zanchi@dvisionlab.com>
2021-04-15 16:18:09 +0200

    feat: activate storeStacks option

--- app/library/dicom/import/options.js
+++ app/library/dicom/import/options.js
@@ -35,13 +35,15 @@ const defaultActions = [
     disabled: true,
     emitter: "dicom-import-upload",
     hint: "series will be saved in your dashboard",
-    storeStacks: false,
+    cacheStacks: false,
+    storeStacks: true,
     text: "upload series"
   },
   {
     disabled: true,
     emitter: "dicom-import-upload-and-open",
     hint: "series will be saved in your dashboard",
+    cacheStacks: true,
     storeStacks: true,
     text: "upload series and open viewer"
   },
@@ -51,6 +53,7 @@ const defaultActions = [
     emitter: "dicom-import-open",
     hint:
       "you won't be able to access these series again once the browser session will be lost",
+    cacheStacks: true,
     storeStacks: true,
     text: "open viewer without uploading"
   }
