3107831faca27b1b50eb2f497eb3ba8d94891739
zansa <sara.zanchi@dvisionlab.com>
2021-04-23 11:21:24 +0200

    feat: example improvements

---
+++ app/src/examples/wireframes/basic/OtherNavLeft.vue
@@ -0,0 +1,9 @@
+<template>
+  <div><b>OTHER</b> nav left (dark: {{ dark }})</div>
+</template>
+
+<script>
+export default {
+  props: { dark: { default: false, type: Boolean } }
+};
+</script>
--- app/src/examples/wireframes/basic/Root.vue
+++ app/src/examples/wireframes/basic/Root.vue
@@ -1,11 +1,36 @@
 <template>
   <basic-wireframe v-bind="options">
-    <router-view />
+    <!-- default wireframe slot -->
+    <div>
+      Current route: {{ $route.name }}
+      <v-btn
+        @click="
+          $router.push({
+            name:
+              $route.name == 'basic-wireframe-home'
+                ? 'basic-wireframe-other'
+                : 'basic-wireframe-home'
+          })
+        "
+      >
+        switch route
+      </v-btn>
+      <!-- route dependant wiereframe components: defined in router -->
+      <router-view />
+    </div>
 
+    <!-- named wireframe slots -->
     <template v-slot:bar="{ className, dark, mobile }">
+      <!-- fixed wireframe components: imported here -->
       <app-bar :class="className" :dark="dark" :mobile="mobile" />
     </template>
-    <template v-slot:navLeft="{ dark }">Left (dark: {{ dark }})</template>
+
+    <template v-slot:navLeft="{ dark }">
+      <!-- route dependant wiereframe components: defined in router -->
+      <router-view name="navLeft" :dark="dark" />
+    </template>
+
+    <!-- fixed wireframe components: imported here -->
     <template v-slot:navRight="{ dark }">Right (dark: {{ dark }})</template>
     <template v-slot:footer>Footer</template>
   </basic-wireframe>
