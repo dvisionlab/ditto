072f903de1de66b0cf8ef0dfb67e922e906fac46
zansa <77335539+zansa@users.noreply.github.com>
2021-05-24 00:17:02 +0200

    Dicom doc (#38)
    * feat: update dicom doc
    
    * feat: add dicom examples

--- app/src/examples/auth/Wrapper.vue
+++ app/src/examples/auth/Wrapper.vue
@@ -1,7 +1,7 @@
 <template>
-  <div :style="{ padding: '10%' }">
+  <v-app :style="{ padding: '10%' }">
     <div class="header black">
-      <div class="app-name mt-4 white--text text-center">
+      <div class="white--text text-center">
         <div>DITTO AUTH</div>
       </div>
     </div>
@@ -9,7 +9,7 @@
     <div :style="{ marginTop: '100px' }">
       <router-view :style="{ flexBasis: '400px' }" />
     </div>
-  </div>
+  </v-app>
 </template>
 
 <style>
