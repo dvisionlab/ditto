6476ced6c1199655d25414eadba418fad6416e4c
Sara Zanchi <sara.zanchi@dvisionlab.com>
2023-02-23 19:01:20 +0100

    fix: overwrite only anonymize tags

--- app/library/dicom/utils.js
+++ app/library/dicom/utils.js
@@ -7,6 +7,55 @@ import * as lt from "larvitar";
 import { saveAs } from "file-saver";
 import print from "print-js";
 
+// Private methods
+// ---------------
+
+const ANONYMIZED_TAGS = [
+  "x00080014", // Instance Creator UID
+  "x00080050", // Accession Number
+  "x00080080", // Institution Name
+  "x00080081", // Institution Address
+  "x00080090", // Referring Physician's Name
+  "x00080092", // Referring Physician's Address
+  "x00080094", // Referring Physician's Telephone numbers
+  "x00081010", // Station Name
+  "x00081030", // Study Description
+  "x0008103e", // Series Description
+  "x00081040", // Institutional Department name
+  "x00081048", // Physician(s) of Record
+  "x00081050", // Performing Physicians' Name
+  "x00081060", // Name of Physician(s) Reading study
+  "x00081070", // Operator's Name
+  "x00081080", // Admitting Diagnoses Description
+  "x00082111", // Derivation Description
+  "x00100010", // Patient's Name
+  "x00100020", // Patient ID
+  "x00100030", // Patient's Birth Date
+  "x00100032", // Patient's Birth Time
+  "x00100040", // Patient's Sex
+  "x00101000", // Other Patient Ids
+  "x00101001", // Other Patient Names
+  "x00101010", // Patient's Age
+  "x00101020", // Patient's Size
+  "x00101030", // Patient's Weight
+  "x00101090", // Medical Record Locator
+  "x00102160", // Ethnic Group
+  "x00102180", // Occupation
+  "x001021b0", // Additional Patient's History
+  "x00104000", // Patient Comments
+  "x00181000", // Device Serial Number
+  "x00181030", // Protocol Name
+  "x00200010", // Study ID
+  "x00200052", // Frame of Reference UID
+  "x00200200", // Synchronization Frame of Reference UID
+  "x00204000", // Image Comments
+  "x00400275", // Request Attributes Sequence
+  "x0040a124", // UID
+  "x00880140", // Storage Media File-set UID
+  "x30060024", // Referenced Frame of Reference UID
+  "x300600c2" // Related Frame of Reference UID
+];
+
 // Public methods
 // --------------
 
@@ -41,7 +90,9 @@ export const anonymizeSeriesStack = (stack, extractMetadata = []) => {
     anonymizedStack.instances[Object.keys(anonymizedStack.instances)[0]]
       .metadata;
   const anonymizedStackMeta = extractMetadata.reduce((result, k) => {
-    result[k] = meta[k];
+    if (ANONYMIZED_TAGS.find(t => t === k)) {
+      result[k] = meta[k];
+    }
     return result;
   }, {});
 
